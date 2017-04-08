8cceff2f44cf460854bdb2eb470909e4f9670a44
benwebsta <bmwebster@wisc.edu>
2017-04-08 12:53:51 -0500

    Employee service, dao, and bean set up

---
+++ src/main/java/com/revature/dao/EmployeeDaoImpl.java
@@ -0,0 +1,19 @@
+package com.revature.dao;
+
+import com.revature.beans.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Override
+	public Employee getEmployeeById(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployeeByLogin(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.revature.service;
+
+import com.revature.beans.Employee;
+
+public class EmployeeService {
+
+	public Employee loginEmployee(String username, String password){
+		
+		return null;
+	}
+	
+	public Employee getEmployeeById(int employeeId){
+		
+		return null;
+	}
+	
+}
