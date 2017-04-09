370f581ebe069d8dafb8d00794cd5b3ba90cab13
Aaron <Cammaaron@gmail.com>
2017-04-09 18:00:24 -0400

    Added Group Service Test

--- src/test/java/com/revature/web/GenderSeviceTest.java
+++ src/test/java/com/revature/web/GenderSeviceTest.java
@@ -23,6 +23,7 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import com.revature.beans.Employee;
+import com.revature.beans.Gender;
 import com.revature.dao.ApartmentDao;
 import com.revature.dao.EmployeeDao;
 import com.revature.dao.GenderDao;
@@ -39,5 +40,67 @@ public class GenderSeviceTest {
 	@Mock
 	GenderDao mockDao;
 	
+	List<Gender> genders;
+	
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks(mockDao);
+		
+		setPreExistingData();
+		mockGetGenderById();
+		
+	}
+
+	private void mockGetGenderById() {
+		when(mockDao.getGenderById(anyInt())).thenReturn(null);
+		
+		for(Gender gender : genders){
+			when(mockDao.getGenderById(gender.getGenderId())).thenReturn(gender.getGender());
+		}
+	}
+
+	private void setPreExistingData() {
+		Gender male = new Gender();
+		male.setGender("Male");
+		male.setGenderId(1);
+		
+		Gender female = new Gender();
+		female.setGender("Female");
+		female.setGenderId(2);
+		
+		Gender coEd = new Gender();
+		coEd.setGender("Co-Ed");
+		coEd.setGenderId(3);
+		
+		Gender other = new Gender();
+		other.setGender("Other");
+		other.setGenderId(4);
+		
+		Gender tumblr = new Gender();
+		tumblr.setGender("Apache Attack Helcopter");
+		tumblr.setGenderId(5);
+		
+		genders = Arrays.asList(male, female, coEd, other, tumblr);
+		
+	}
+	
+	@Test
+	public void testGetGenderById(){
+		String male = genderService.getGenderById(1);
+		String female = genderService.getGenderById(2);
+		String other = genderService.getGenderById(4);
+		String coEd = genderService.getGenderById(3);
+		
+		assertEquals("Male", male);
+		assertEquals("Female", female);
+		assertEquals("Co-Ed", coEd);
+		assertEquals("Other", other);
+	}
+	
+	@Test
+	public void testgetNotExistingGenderById(){
+		String none = genderService.getGenderById(42);
+		assertNull(none);
+	}
 	
 }
