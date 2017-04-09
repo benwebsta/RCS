ce3c81140347f335ead341cb651511df4c79ea12
benwebsta <bmwebster@wisc.edu>
2017-04-09 15:47:32 -0400

    angular example flushed out slightly

--- src/main/webapp/WEB-INF/resources/updatePasswordController.js
+++
@@ -1,9 +0,0 @@
-app.controller('housingItemController', 
-	["$scope",  
- function($scope) {
-	  var passwordUpdate = "nothing";
-	  $scope.update = function(password) {
-		  console.log("test");
-	    $scope.passwordUpdate = password;
-	  }
-}]);
---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,12 @@
+
+var app = angular.module('rcs', []);
+
+
+/*app.config(function($stateProvider, $urlRouterProvider) {
+$stateProvider
+.state('updatePassword', {
+  url: "/updatePassword",
+  templateUrl: "../pages/updatePassword.jsp"
+})
+  $urlRouterProvider.otherwise("/");
+})*/
