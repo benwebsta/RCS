0d48d8d4a5e7ee7d2c92b0f90311b01f98c76c57
Aaron <Cammaaron@gmail.com>
2017-04-11 12:13:01 -0400

    Implemented Missing GroupDao method, added in messing @Component annotations

--- src/main/java/com/revature/service/GenderService.java
+++ src/main/java/com/revature/service/GenderService.java
@@ -1,5 +1,8 @@
 package com.revature.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GenderService {
 	
 	public String getGenderById(int genderId){
--- src/main/java/com/revature/service/GroupService.java
+++ src/main/java/com/revature/service/GroupService.java
@@ -2,33 +2,40 @@ package com.revature.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.revature.beans.Employee;
 import com.revature.beans.Group;
+import com.revature.dao.GenderDaoImpl;
+import com.revature.dao.GroupDao;
+import com.revature.dao.GroupDaoImpl;
 
+@Component
 public class GroupService {
 
+	GroupDao groupDao = new GroupDaoImpl();
 	
 	public List<Group> getAllGroups() {
 		// TODO Auto-generated method stub
-		return null;
+		return groupDao.getAllGroups(); 
 	}
 
 	
 	public Group getGroupById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		return groupDao.getGroupById(id);
 	}
 
 	
 	public List<Group> getGroupsContainingEmployee(Employee employee) {
 		// TODO Auto-generated method stub
-		return null;
+		return groupDao.getGroupsContainingEmployee(employee);
 	}
 
 	
-	public void addGroup(Employee... employees) {
+	public void addGroup(Group group) {
 		// TODO Auto-generated method stub
-		
+		groupDao.addGroup(group);
 	}
 	
 }
