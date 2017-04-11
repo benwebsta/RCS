fbab932e6d429504cbb2afd90ccc68489fdadb18
benwebsta <bmwebster@wisc.edu>
2017-04-10 22:12:02 -0400

    added state to app.js

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -4,9 +4,14 @@ var app = angular.module('rcs', []);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 $stateProvider
+$stateProvider
+.state('home', {
+  url: "/home",
+  templateUrl: "/home.jsp"
+})
 .state('updatePassword', {
   url: "/updatePassword",
   templateUrl: "/updatePassword.jsp"
 })
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise("/home.jsp");
 })
