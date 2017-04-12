3cd9643514c324637cf9435225439ea7b71ef70c
benwebsta <bmwebster@wisc.edu>
2017-04-11 22:00:20 -0400

    working on emailing

--- src/main/webapp/resources/js/sendHousingRequestController.js
+++ src/main/webapp/resources/js/sendHousingRequestController.js
@@ -3,6 +3,31 @@ app.controller("sendHousingRequestController",
 		 function($scope, $http) {
 		$('#sendHousingRequestModal').modal({ show: false})
 		
+		$scope.test = function() {
+			console.log("test");
+		}
+		
+		
+		$scope.send = function() {
+			console.log("teststststuuuu");
+		    
+		    var recipientEmail = $scope.recipientEmail;
+		    var header = $scope.header;
+		    var body = $scope.body;
+		    
+		    console.log(recipientEmail);
+		    console.log(header);
+		    console.log(body);
+		    
+		    var email = {
+		    		recipientEmail : recipientEmail,
+		    		header : header, 
+		    		body : body
+		    }
+		    console.log(email);
+	
+		  }
+		
 		  $scope.sendHousingRequest = function() {
 			  $('#sendHousingRequestModal').modal('show');
 		  }
