a517961e5a9ae1299a81a40a0e0ea892ae76c560
benwebsta <bmwebster@wisc.edu>
2017-04-13 15:05:58 -0400

    just pushing to save

--- src/main/java/com/revature/service/HRService.java
+++ src/main/java/com/revature/service/HRService.java
@@ -9,22 +9,28 @@ import com.revature.dao.ApartmentDao;
 import com.revature.dao.ApartmentDaoImpl;
 import com.revature.dao.EmployeeDao;
 import com.revature.dao.EmployeeDaoImpl;
+import com.revature.dao.GenderDao;
+import com.revature.dao.GenderDaoImpl;
 
 @Component
 public class HRService {
 
 	EmployeeDao employeeDao = new EmployeeDaoImpl();
 	ApartmentDao apartmentDao = new ApartmentDaoImpl();
+	GenderDao genderDao = new GenderDaoImpl();
 	
 	public List<Employee> getAllEmployeesInHousing(){
 		List<Employee> employees = employeeDao.getAllEmployees();
 		employees.removeAll(employees);
 		
 		List<Employee> tempEmployees = employeeDao.getAllEmployees();
+		
 		System.out.println(tempEmployees);
 		
 		for(Employee employee : tempEmployees){
 			if(employee.getFill() == 1){
+				/*employee.setApartment(apartmentDao.getApartmentById(employee.getApartmentId()));
+				employee.setGender(genderDao.getGenderById(employee.getGenderId()));*/
 				employees.add(employee);
 			}
 		}
